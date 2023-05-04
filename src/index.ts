e2e966085340b3bce01a23cfaa78e5f17a859466
Mariana Tamay <maritamay4@gmail.com>
2023-05-04 09:50:42 -0300

    Adição do cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as express from "express";
 import * as dotenv from "dotenv";
 import routes from "./routes";
+import{Request} from"express";
+import * as cors from'cors';
 
 dotenv.config();
 
@@ -8,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors<Request>());
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
 
 app.use(routes);
